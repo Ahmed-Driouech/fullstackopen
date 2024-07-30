23f7f43666d3ecf7afdc4985b153d3099309b0bf
Ahmed-Driouech <a.a.driouech@student.vu.nl>
2024-07-30 22:58:17 +0200

    partial 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -8,17 +8,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGood = () =>{
     setGood(good + 1)
+    handleStatistics()
   }
-
+  
   const handleNeutral = () =>{
     setNeutral(neutral + 1)
+    handleStatistics()
   }
-
+  
   const handleBad = () =>{
     setBad(bad + 1)
+    handleStatistics()
+  }
+  
+  const handleStatistics=()=>{
+    setAll(good + neutral + bad)
+    setAverage((good + (bad * -1))/all)
+    setPositive((good/all)*100)
   }
 
   return (
@@ -31,6 +43,9 @@ const App = () => {
       <Statistics text="good" value={good}/>
       <Statistics text="neutral" value={neutral}/>
       <Statistics text="bad" value={bad}/>
+      <Statistics text="all" value={all}/>
+      <Statistics text="average" value={average}/>
+      <Statistics text="positive" value={positive}/>
     </div>
   )
 }
