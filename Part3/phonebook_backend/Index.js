7ef21c3a488a01f8399995ba7674730926e75354
Ahmed-Driouech <a.a.driouech@student.vu.nl>
2025-02-05 21:48:24 +0100

    3.8*

--- Part3/phonebook_backend/Index.js
+++ Part3/phonebook_backend/Index.js
@@ -3,30 +3,32 @@ const morgan = require('morgan')
 const app = express()
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 //persons cant be const, otherwise we cant make changes to it with http requests
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
