51f19bbff4d37a768045b3742ce59a3597718562
Ahmed-Driouech <a.a.driouech@student.vu.nl>
2025-03-01 22:08:43 +0100

    3.11

--- Part3/index.js
+++ Part3/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -27,7 +26,7 @@ let persons = [
 ]
 //persons cant be const, otherwise we cant make changes to it with http requests
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body'))
