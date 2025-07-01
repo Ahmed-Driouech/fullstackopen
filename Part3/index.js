f045b493f790f7bdad359e7641064b36a0d3e405
Ahmed Driouech <A.A.Driouech-1@student.tudelft.nl>
2025-07-02 00:01:02 +0200

    3.15-3.18*

--- Part3/index.js
+++ Part3/index.js
@@ -36,42 +36,61 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
       response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', async(request, response) => {
     const time = new Date()
+    const dbSize = await Person.countDocuments({})
     response.send(
-        `<p>Phonebook has info for 2 people</p>
+        `<p>Phonebook has info for ${dbSize} people</p>
          <p>${time}</p>`)
   })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 const id = request.params.id
-const person = Person.findById(id).then(person => {
-  response.json(person)
+Person.findById(id)
+.then(person => {
+  if(person){
+    return response.json(person)
+  }
+  else{
+    return response.status(404).end()
+  }
+})
+.catch(error => next(error))
 })
 
-if(person){
-    response.json(person)
-}
-else{
-    console.log('person not found!')
-    response.status(404).end()
-}
-})
-
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+async function updatePerson (request, response, next) {
+  const {name, number} = request.body
+
+  Person.findById(request.params.id)
+  .then(person => {
+    if(!person){
+      response.status(404).end()
+    }
+
+    person.name = name
+    person.number = number
+
+    return person.save().then((updatedPerson) => response.json(updatedPerson))
+  })
+  .catch(error => next(error))
+}
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if(!body.name){
@@ -86,24 +105,40 @@ app.post('/api/persons', (request, response) => {
           })
     }
 
-    //const duplicateName = Person.find({name: body.name})
-
-   /* if(duplicateName){
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-          })
-    }*/
+    Person.find({name: body.name})
+    .then(person => {
+      if(person){
+        updatePerson(request,response,next)
+        return console.log("Person updated!")
+      }
+    })
+    .catch(error => next(error))
 
     const newPerson = new Person({
         name: body.name,
         number: body.number
     })
 
-    newPerson.save().then(savedPerson => {
+   return newPerson.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', updatePerson)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
